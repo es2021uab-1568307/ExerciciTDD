2ac22b0a11ad9f3c6fed6312b5867a752aaf0920
1568307 <borja.arcos@e-campus.uab.cat>
2021-03-20 13:10:39 +0100

    test fifteen-all recoded

--- TDD/Test/tennis_test.py
+++ TDD/Test/tennis_test.py
@@ -4,9 +4,48 @@ Created on Sat Mar 20 11:30:01 2021
 
 @author: borja
 """
+import pytest
 
-from main.game import *
+from main.game import game
 
-def test_startOfGame():
-    result = get_score()
+@pytest.fiture()
+def setup() -> game:
+    return game("Toni", "Carla")
+@pytest.fixture()
+def teardown():
+    return
+
+"""@pytest.mark.parametrize("score_player_1, score_player_2, expected_result", [
+    (0, 0, "love-all")
+    ]
+    ),
+"""
+def test_startOfGame(setup):
+    game = setup
+    result = game.get_score()
     assert result == "love-All"
+    
+def test_fifteenAll_afterPlayer1Scores(setup):
+    game = setup
+    
+    game.wins_point("Toni")
+    result = game.get_score()
+    assert result == "fifteen-all"
+    
+def test_Player1AndPlayer2onePoint_fifteenAll(setup):
+    game = setup
+    
+    game.wins_point("Toni")
+    game.wins_point("Carla")
+    result = game.get_score()
+    
+    assert result == "Fifteen-all"
+    
+def test_Player1TwoPointsAndPlayer2onePoint(setup):
+    game = setup
+    
+    game.wins_point("Toni")
+    game.wins_point("Carla")
+    result = game.get_score()
+    
+    assert result == "thirty-fifteen"
--- TDD/main/game.py
+++ TDD/main/game.py
@@ -4,6 +4,9 @@ Created on Sat Mar 20 10:32:15 2021
 
 @author: borja
 """
+
+    
+
 class game:
     
     def __init__(self, player1:str, player2: str):
@@ -16,5 +19,7 @@ class game:
         
     def get_score(self) -> str:
         if self._wins == self.player1:
+            return "Fifteen-love"
+        if self._wins == self.player2:
             return "Fifteen-love"
         return "love-All"
