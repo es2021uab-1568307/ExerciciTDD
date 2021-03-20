77c6dee182053db2f82752348dad5cc84877f279
1568307 <borja.arcos@e-campus.uab.cat>
2021-03-20 14:04:23 +0100

    trying to fix parametrized test without luck :(

--- TDD/Test/tennis_test.py
+++ TDD/Test/tennis_test.py
@@ -6,31 +6,29 @@ Created on Sat Mar 20 11:30:01 2021
 """
 import pytest
 
-from main.game import game
+from main.game import *
 
-@pytest.fiture()
-def setup() -> game:
-    return game("Toni", "Carla")
+@pytest.fixture()
+def setup() -> player:
+    return player1("Toni"),player2("Carla")
 @pytest.fixture()
 def teardown():
     return
 
-"""@pytest.mark.parametrize("score_player_1, score_player_2, expected_result", [
-    (0, 0, "love-all")
-    ]
-    ),
-"""
+@pytest.mark.parametrize("score_player_1, score_player_2, expected_result", [
+    (0, 0, "love-all")]
+    )
 def test_startOfGame(setup):
-    game = setup
-    result = game.get_score()
-    assert result == "love-All"
+
+    result = get_score()
+    assert result == "love-all"
     
 def test_fifteenAll_afterPlayer1Scores(setup):
     game = setup
     
     game.wins_point("Toni")
     result = game.get_score()
-    assert result == "fifteen-all"
+    assert result == "fifteen-love"
     
 def test_Player1AndPlayer2onePoint_fifteenAll(setup):
     game = setup
@@ -39,13 +37,13 @@ def test_Player1AndPlayer2onePoint_fifteenAll(setup):
     game.wins_point("Carla")
     result = game.get_score()
     
-    assert result == "Fifteen-all"
+    assert result == "fifteen-all"
     
-def test_Player1TwoPointsAndPlayer2onePoint(setup):
-    game = setup
+def test_Player1TwoPoints_thirtyLove(setup):
+    game = setup()
     
     game.wins_point("Toni")
-    game.wins_point("Carla")
+    game.wins_point("Toni")
     result = game.get_score()
     
-    assert result == "thirty-fifteen"
+    assert result == "thirty-love"
--- TDD/main/game.py
+++ TDD/main/game.py
@@ -5,21 +5,22 @@ Created on Sat Mar 20 10:32:15 2021
 @author: borja
 """
 
+class player:
+    def __init__(self, name):
+        self._name = name
+        self._points = 0
+    
+    def wins_point(self, player: str):
+        self._points += 1
+        
+    def get_score(player1, player2) -> str:
+        if player1._points == 1 and player2._points == 0:
+            return "fifteen-love"
+        if player1._points == 2:
+            return "thirty-love"
+        if player2._points == 1:
+            return "fifteen-all"
+        return "love-all"
+
     
 
-class game:
-    
-    def __init__(self, player1:str, player2: str):
-        self._player1 = player1
-        self._player2 = player2
-        self._wins = ""
-        
-    def wins_point(self, player: str):
-        self._wins = player
-        
-    def get_score(self) -> str:
-        if self._wins == self.player1:
-            return "Fifteen-love"
-        if self._wins == self.player2:
-            return "Fifteen-love"
-        return "love-All"
