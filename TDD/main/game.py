61bce2f51e075592e5b3d6e917383b71c4c6da86
1568307 <borja.arcos@e-campus.uab.cat>
2021-03-20 12:31:28 +0100

    test fifteen-all

--- TDD/main/game.py
+++ TDD/main/game.py
@@ -4,6 +4,17 @@ Created on Sat Mar 20 10:32:15 2021
 
 @author: borja
 """
-
-def get_score():
-    return "love-All"
+class game:
+    
+    def __init__(self, player1:str, player2: str):
+        self._player1 = player1
+        self._player2 = player2
+        self._wins = ""
+        
+    def wins_point(self, player: str):
+        self._wins = player
+        
+    def get_score(self) -> str:
+        if self._wins == self.player1:
+            return "Fifteen-love"
+        return "love-All"
